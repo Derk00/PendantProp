7e4e61fbe484da4c3b4095136824ac5f9917441b
Pim Dankloff <pimdankloff@gmail.com>
2024-12-10 16:42:43 +0100

    improvements

--- server/custom_functions.py
+++ server/custom_functions.py
@@ -1,5 +1,10 @@
+import os
+
 def measure_well(start_well:str, end_well:str):
     print(f"Measuring well from {start_well} to {end_well}")
 
-def save_settings(exp_name:str):
-    print(f"Saving settings for experiment {exp_name}")
+def save_csv_file(exp_name:str, csv_file, app):
+    exp_dir = os.path.join(app.config["UPLOAD_FOLDER"], exp_name)
+    os.makedirs(exp_dir, exist_ok=True)
+    file_path = os.path.join(exp_dir, csv_file.filename)
+    csv_file.save(file_path)
